b0a7994a7b3fba7b190efc98d681ad5badab6a27
David Ansermino <david.ansermino@mail.utoronto.ca>
2018-07-16 17:15:46 -0400

    Added solc-js deploy script "

---
+++ scripts/solc-js-publish.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Check if version is new release
+SOLC_JS_VERSION=`npm view @shyftnetwork/shyft_solc version`
+SOLC_VERSION=`node -e "console.log(require('../bin/list.json').latestRelease);"`
+if [ $SOLC_JS_VERSION == $SOLC_VERSION ]
+then
+    exit 0
+fi
+
+# Add git ssh key
+openssl aes-256-cbc -K $encrypted_c6ed341a5849_key -iv $encrypted_c6ed341a5849_iv -in shyft_deploy_key.enc -out shyft_deploy_key -d
+chmod 600 shyft_deploy_key
+eval `ssh-agent -s`
+ssh-add shyft_deploy_key
+
+# Clone solc-js
+git clone --depth 2 git@github.com:shyftnetwork/shyft_solc-js.git
+cd shyft_solc-js
+git config user.name "travis"
+git config user.email "david@chainsafe.io"
+git clean -f -d -x
+
+# Update package version
+npm version patch
+NEWVERSION=`node -e "console.log(require('./package.json').version);"`
+
+# Call npm publish, prepublish will fetch latest version
+npm publish --access=public
+
+# Push changes to git
+git add package.json
+git commit -a -m "Updated to version $NEWVERSION"
+git push origin master
